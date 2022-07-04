12f3a2bebf31c4d732d9456c96b14ba00dec347d
shpertakhia <shpertakhia@napr.gov.ge>
2022-07-05 02:12:37 +0400

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+//Variables
+const redberry_knight_cup_landing = document.querySelector(".frame");
+const rectangle1 = document.querySelector(".right-container");
+const rectangle2 = document.querySelector(".header-container");
+const rectangle3 = document.querySelector(".footer-container");
+const landing_page_scrolling_photo = document.querySelector(".left-container");
