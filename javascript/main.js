481f2e2f58ccdcfb3b6a7c1e9195669b29cdb05a
shpertakhia <shpertakhia@napr.gov.ge>
2022-07-06 14:44:37 +0400

    third page start

--- javascript/main.js
+++ javascript/main.js
@@ -4,3 +4,16 @@ const rectangle1 = document.querySelector(".right-container");
 const rectangle2 = document.querySelector(".header-container");
 const rectangle3 = document.querySelector(".footer-container");
 const landing_page_scrolling_photo = document.querySelector(".left-container");
+
+
+let url = "https://chess-tournament-api.devtest.ge/api/grandmasters";
+
+async function fetchAsync (url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    return data;
+}
+
+fetchAsync(url);
+
